65f633f3fc314e47ad2fba9dcf8b10678b2443d1
YNGHK <celebi3@naver.com>
2025-05-04 23:15:59 +0900

    My i IntroDemo

--- IntroDemo/IntroDemo/ContentView.swift
+++ IntroDemo/IntroDemo/ContentView.swift
@@ -8,14 +8,133 @@
 import SwiftUI
 
 struct ContentView: View {
+    @State private var selectedItem: Item = items.first!
+    @State private var introItems: [Item] = items
+    @State private var activeIndex: Int = 0
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+        VStack(spacing: 0) {
+            // Back Button
+            Button {
+                updateItem(isForward: false)
+            } label: {
+                Image(systemName: "chevron.left")
+                    .font(.title3.bold())
+                    .foregroundStyle(.indigo.gradient)
+                    .contentShape(.rect)
+            }
+            .padding(15)
+            .frame(maxWidth: .infinity, alignment: .leading)
+            // Only visible from second item
+            .opacity(selectedItem.id != introItems.first?.id ? 1 : 0)
+            ZStack {
+                // Animated Icons
+                ForEach(introItems) { item in
+                    AnimatedIconView(item)
+                }
+            }
+            .frame(height: 250)
+            .frame(maxHeight: .infinity)
         }
-        .padding()
+        VStack(spacing: 6) {
+            // Progress Indicator View
+            HStack(spacing: 4) {
+                ForEach(introItems) { item in
+                    Capsule()
+                        .fill(selectedItem.id == item.id ? Color.primary : .gray)
+                        .frame(width: selectedItem.id == item.id ? 25:4, height: 4)
+                }
+            }
+            .padding(.bottom, 15)
+            
+            Text(selectedItem.title)
+                .font(.title.bold())
+                .contentTransition(.numericText())
+            
+            // Description
+            Text("우리는 죠습니다")
+                .font(.caption2)
+                .foregroundStyle(.gray)
+            
+            // Next/continue Button
+            Button {
+                updateItem(isForward: true)
+            } label: {
+                Text(selectedItem.id == introItems.last!.id ? "Continue" : "Next")
+                    .fontWeight(.semibold)
+                    .foregroundStyle(.white)
+                    .frame(width: 250)
+                    .padding(.vertical, 12)
+                    .background(.indigo.gradient, in: .capsule)
+            }
+            .padding(.top, 25)
+        }
+        .multilineTextAlignment(.center)
+        .frame(width: 300)
+        .frame(maxHeight: .infinity)
+    }
+    
+    @ViewBuilder
+    func AnimatedIconView(_ item: Item) -> some View {
+        let isSelected = selectedItem.id == item.id
+        
+        Image(systemName: item.image)
+            .font(.system(size: 80))
+            .foregroundStyle(.white.shadow(.drop(radius: 10)))
+            .blendMode(.overlay)
+            .frame(width: 120, height: 120)
+            .background(.indigo.gradient, in: .rect(cornerRadius: 32))
+            .background {
+                RoundedRectangle(cornerRadius: 35)
+                    .fill(.background)
+                    .shadow(color: .primary.opacity(0.2), radius: 1, x: 1, y: 1)
+                    .shadow(color: .primary.opacity(0.2), radius: 1, x: -1, y: -1)
+                    .padding(-3)
+                    .opacity(selectedItem.id == item.id ? 1 : 0)
+            }
+        
+        // Reset Location
+            .rotationEffect(.init(degrees: -item.rotation))
+            .scaleEffect(isSelected ? 1.1 : item.scale, anchor: item.anchor)
+            .offset(x: item.offset)
+            .rotationEffect(.init(degrees: item.rotation))
+        
+        // Placing active icon at the top
+            .zIndex(isSelected ? 2 : item.zIndex)
+    }
+    
+    func updateItem(isForward: Bool) {
+        guard isForward ? activeIndex != introItems.count - 1 : activeIndex != 0 else { return }
+        var fromIndex: Int = 0
+        
+        // To index
+        if isForward {
+            activeIndex += 1
+        } else {
+            activeIndex -= 1
+        }
+        //From index
+        if isForward {
+            fromIndex = activeIndex - 1
+        } else {
+            fromIndex = activeIndex + 1
+        }
+        
+        // Shifting from and to icon locations
+        withAnimation(.bouncy(duration: 1)) {
+            introItems[fromIndex].scale = introItems[activeIndex].scale
+            introItems[fromIndex].rotation = introItems[activeIndex].rotation
+            introItems[fromIndex].anchor = introItems[activeIndex].anchor
+            introItems[fromIndex].offset = introItems[activeIndex].offset
+            
+            // To location is always at the center
+            introItems[activeIndex].scale = 1
+            introItems[activeIndex].rotation = .zero
+            introItems[activeIndex].anchor = .center
+            introItems[activeIndex].offset = .zero
+            
+            // Updating Selected Item
+            selectedItem = introItems[activeIndex]
+        }
     }
 }
 
--- IntroDemo/IntroDemo/Model/Item.swift
+++ IntroDemo/IntroDemo/Model/Item.swift
@@ -12,6 +12,7 @@ struct Item: Identifiable {
     var image: String
     var title: String
     
+    // Locations of each icons in the view
     var scale: CGFloat = 1
     var anchor: UnitPoint = .center
     var offset: CGFloat = 0
@@ -21,37 +22,37 @@ struct Item: Identifiable {
 
 let items: [Item] = [
     .init(
-        image: "figure.walk.circle.fill",
-        title: "Keep an eye on your workout",
+        image: "waveform.path.ecg.magnifyingglass",
+        title: "Analzye the crying of your baby",
         scale: 1
     ),
     .init(
-        image: "figure.run.circle.fill",
-        title: "Keep an eye on your workout",
+        image: "calendar.badge.plus",
+        title: "Manage your babysitting schedule effectively",
         scale: 0.6,
         anchor: .topLeading,
         offset: -70,
         rotation: 30
     ),
     .init(
-        image: "figure.badminton.circle.fill",
-        title: "Keep an eye on your workout",
+        image: "clock.fill",
+        title: "Keep track of your baby's activity time",
         scale: 0.5,
         anchor: .bottomLeading,
         offset: -60,
         rotation: -35
     ),
     .init(
-        image: "figure.climnbing.circle.fill",
-        title: "Keep an eye on your workout",
+        image: "person.text.rectangle.fill",
+        title: "Manage your baby's profile cards",
         scale: 0.4,
         anchor: .bottomLeading,
         offset: -50,
         rotation: 160
     ),
     .init(
-        image: "figure.cooldown.circle.fill",
-        title: "Keep an eye on your workout",
+        image: "microphone.fill",
+        title: "Touchfree voice control",
         scale: 0.35,
         anchor: .bottomLeading,
         offset: -50,
