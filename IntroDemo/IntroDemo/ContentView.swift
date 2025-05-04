0950da489e221189addbf222578e8bec928b4472
YNGHK <celebi3@naver.com>
2025-05-04 21:27:02 +0900

    IntroDemo

---
+++ IntroDemo/IntroDemo/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  IntroDemo
+//
+//  Created by Yung Hak Lee on 5/4/25.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ IntroDemo/IntroDemo/IntroDemoApp.swift
@@ -0,0 +1,17 @@
+//
+//  IntroDemoApp.swift
+//  IntroDemo
+//
+//  Created by Yung Hak Lee on 5/4/25.
+//
+
+import SwiftUI
+
+@main
+struct IntroDemoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
